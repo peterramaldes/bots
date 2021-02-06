28f72b81bce76e7ea66f09beec82dd5f30fad25f
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2021-02-06 12:24:25 -0300

    Adicionado audio para o joão.

--- index.js
+++ index.js
@@ -8,25 +8,37 @@ dotenv.config();
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
 	let username = newState.member.displayName;
-	let connection;
 
-	if (newState.channel === undefined) return
-	if (oldState.channel && oldState.channel.name !== 'AFK') return
-	if (newState.channel && newState.channel.name === 'AFK') return 
-	if (username === 'Detector de Viadão') return
+	if (newState.channel === undefined) return;
+	if (oldState.channel && oldState.channel.name !== 'AFK') return;
+	if (newState.channel && newState.channel.name === 'AFK') return;
+	if (username === 'Detector de Viadão') return;
 
-	connection = await newState.member.voice.channel.join();
+	newState.member.voice.channel
+		.join()
+		.then((connection) => playSoundByUser(username, connection, newState));
+});
 
-	if (connection) {
-		let dispatcher = connection.play('./sound.mp3', {
-			volume: 1.0
+function playSoundByUser(username, connection, newState) {
+	if (!username) return;
+
+	let dispatcher;
+
+	if (username === 'João V.') {
+		dispatcher = connection.play('./joao.mp3', {
+			volume: 1.0,
 		});
-		dispatcher.on('finish', () => {
-			// Remove o bot da sala
-			newState.member.voice.channel.leave();
-			dispatcher.destroy();
+	} else {
+		dispatcher = connection.play('./sound.mp3', {
+			volume: 1.0,
 		});
 	}
-});
+
+	dispatcher.on('finish', () => {
+		// Remove o bot da sala
+		dispatcher.destroy();
+		newState.member.voice.channel.leave();
+	});
+}
 
 client.login(process.env.TOKEN);
